ae1311b46dc7d4f4dffda547cf10dd00679765b9
“JANK-L” <jankevilu+git@gmail.com>
2025-09-10 19:56:50 +0300

    WIP: initial Express backend server

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const server = express();
+
+//Conection to DB
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      console.log("-----");
+      console.log("Connected to DB.");
+      console.log(`Listening on port ${process.env.PORT}.`);
+      console.log(`Hosting: https://localhost:${process.env.PORT}`);
+      console.log("-----");
+    });
+  })
+  .catch((err) => console.log(err));
+
+//Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Method: ", req.method);
+  console.log("Path: ", req.path);
+  console.log("-----");
+  next();
+});
